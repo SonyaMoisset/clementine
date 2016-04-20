9f0aa57ca99af78dbbfe27adacc9254ad2fca2dc
SonyaMoisset <sonya.moisset@gmail.com>
2016-04-21 02:36:10 +0900

    Update server.js

--- server.js
+++ server.js
@@ -8,20 +8,19 @@ var app = express();
 
 mongo.connect('mongodb://localhost:27017/clementinejs', function (err, db) {
 
-if (err) {
-  throw new Error('Database failed to connect!');
-} else {
-  console.log('MongoDB successfully connected on port 27017.');
-}
+    if (err) {
+        throw new Error('Database failed to connect!');
+    } else {
+        console.log('MongoDB successfully connected on port 27017.');
+    }
 
-  app.use('/public', express.static(process.cwd() + '/public'));
+    app.use('/public', express.static(process.cwd() + '/public'));
+    app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 
-  app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
+    routes(app, db);
 
-  routes(app, db);
+    app.listen(3000, function () {
+        console.log('Listening on port 3000...');
+    });
 
-  app.listen(3000, function() {
-    console.log('Listening on port 3000...');
-  });
-  
 });
